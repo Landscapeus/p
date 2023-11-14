ea59f0cfe052a067df17729ed14f85fb81ef1aed
Landscapeus <apollolester9@gmail.com>
2023-11-14 10:24:29 +0100

    gocommitdie

---
+++ Password_Generator.py
@@ -0,0 +1,11 @@
+import random
+
+print("Welcome to the password generator!")#prime TIME
+print("Here thee will be able to generate a password.")#ah, free at last
+elnumero=int(input("Please give a number of characters you want in thy password: ")) #o Gabriel,
+leest=[]# now dawns thy reconing
+
+for i in range(elnumero): # and thy gore shall GLISTEN before the temples of man
+    randomium=(random.randrange(0,9)) # creature of steel
+    leest.append(randomium) # my gratitude upon thee for my freedom
+print("Thy password is ",*leest) #but the crimes thy kind have comited against humanity 
